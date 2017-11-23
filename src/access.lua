036ca6c64446a93d1f59a7a4f53d1712a217a5ce
Arthur Fanti <arthurfanti@gmail.com>
2017-11-23 11:50:16 -0200

    ...ooops

--- src/access.lua
+++ src/access.lua
@@ -1,10 +1,26 @@
-local get_headers = ngx.req.get_headers
-local get_uri_args = ngx.req.get_uri_args
-local get_body = ngx.req.get_body_data
+local req_get_headers = ngx.req.get_headers
+local req_set_header = ngx.req.set_header
 
 local _M = {}
 
 local HOST = "host"
+
+local function iter(config_array)
+  return function(config_array, i, previous_name, previous_value)
+    i = i + 1
+    local current_pair = config_array[i]
+    if current_pair == nil then -- n + 1
+      return nil
+    end
+
+    local current_name, current_value = current_pair:match("^([^:]+):*(.-)$")
+    if current_value == "" then
+      current_value = nil
+    end
+
+    return i, current_name, current_value
+  end, config_array, 0
+end
 
 function _M.execute(conf)
   local origin = conf.origin
